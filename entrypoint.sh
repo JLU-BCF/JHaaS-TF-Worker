695bbd72575275b1f3b17513e497a7e481b355a8
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-05-15 09:39:53 +0200

    make secrets mount point configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-# Do nothing, just print out the environment and names of attached secrets
+# Do nothing, just print out the environment and names and hashes of attached secrets
 env
-[ -d /run/secrets ] && ls /run/secrets || echo 'no secrets attached'
+
+SECRETS_PATH="${SECRETS_PATH:-/secrets}"
+
+if [ -d "$SECRETS_PATH" ]; then
+  echo "-----------"
+  ls -l "$SECRETS_PATH"
+  echo "-----------"
+  for SECRET in "$SECRETS_PATH"/*; do
+    sha256sum "$SECRET";
+  done
+  echo "-----------"
+else
+  echo "Directory $SECRETS_PATH does not exist or is not a directory"
+fi
