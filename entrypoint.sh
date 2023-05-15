1307ad12155689e07c7ea4193093c6a0108acf27
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-05-15 13:17:28 +0200

    write something to s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 env
 
 SECRETS_PATH="${SECRETS_PATH:-/secrets}"
+S3_CONF="${S3_CONF:-minio.secret}"
+TF_CONF="${TF_CONF:-terraform.secret}"
 
 if [ -d "$SECRETS_PATH" ]; then
   echo "-----------"
@@ -13,6 +15,10 @@ if [ -d "$SECRETS_PATH" ]; then
     sha256sum "$SECRET";
   done
   echo "-----------"
+  mkdir -p /root/.mc && ln -s "$SECRETS_PATH/$S3_CONF" /root/.mc/config.json
+  echo "it's me: $(hostname)" > demo.txt
+  date >> demo.txt
+  mc -C /root/.mc mv demo.txt s3/tf-state/
 else
   echo "Directory $SECRETS_PATH does not exist or is not a directory"
 fi
